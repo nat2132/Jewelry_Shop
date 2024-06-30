74e7ab3393a2ddff46759e8d1a02747b95b21176
Natoli Adugna <157066283+nat2132@users.noreply.github.com>
2024-06-30 14:03:23 +0300

    Delete Jewelry__Shop directory

--- Jewelry__Shop/info.py
+++
@@ -1,6 +0,0 @@
-EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'tezetatreasures@gmail.com'
-EMAIL_HOST_PASSWORD = 'ftxc bstd odot orsl'
-EMAIL_PORT = 587
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
